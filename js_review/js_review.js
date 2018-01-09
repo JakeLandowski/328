fc514364c88448f19599938b8a5129308207bb83
JakeLandowski <jakelandowski@gmail.com>
2018-01-08 17:23:31 -0800

    Finished part two FizzBuzz

--- js_review/js_review.js
+++ js_review/js_review.js
@@ -9,3 +9,17 @@ function intro()
 {
     console.log('Introduction to Javascript');
 }
+
+function fizzBuzz()
+{
+    for(let i = 1; i <= 100; i++)
+    {
+        let willPrint = '';
+
+        if(i % 3 === 0) willPrint += 'Fizz';
+        if(i % 5 === 0) willPrint += 'Buzz';
+
+            //  if willPrint empty, prints number
+        console.log(willPrint ? willPrint : i);
+    }
+}
